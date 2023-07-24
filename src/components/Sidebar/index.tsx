6d3645f989721779f33b80de14018cf03280d3fd
serhiyhvala <serhiyhvala.code@gmail.com>
2023-07-24 14:17:14 +0300

    Create Sidebar Component

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,3 +1,36 @@
+import { Menu } from "@components/Menu";
+import { ProfileInfo } from "@components/ProfileInfo";
+
+import { Avatar, Logo, Plus } from "@components/Icons";
+
+import styles from "./sidebar.module.scss";
+import { ToggleTheme } from "@components/ToggleTheme";
+import { Link } from "react-router-dom";
+import { Button } from "ui/Button";
+
 export const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <aside className={styles.sidebar}>
+      <div className={styles.logo}>
+        <Link to="/">
+          <Logo />
+        </Link>
+        <Link to="new-article">
+          <Button borderRadius={10}>
+            <Plus />
+            <span>Створити статтю</span>
+          </Button>
+        </Link>
+      </div>
+      <Menu />
+      <ProfileInfo
+        email="jen.wilson@gmail.com"
+        Avatar={Avatar}
+        username="Jenny Wilson"
+      />
+      <div className={styles.theme}>
+        <ToggleTheme />
+      </div>
+    </aside>
+  );
 };
