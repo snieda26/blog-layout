8c49e63debe7a57be5a0ae3e7c63cca815f66dda
serhiyhvala <serhiyhvala.code@gmail.com>
2023-07-24 09:47:28 +0300

    Add React Browser Router

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,12 @@ import ReactDOM from "react-dom/client";
 import { App } from "./App.tsx";
 
 import "./index.scss";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
