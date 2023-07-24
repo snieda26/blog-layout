ff7d6221d547d6d23c7db776385fc83fcec5d08d
serhiyhvala <serhiyhvala.code@gmail.com>
2023-07-24 10:12:41 +0300

    Add Icons To Project

---
+++ src/components/Icons/index.ts
@@ -0,0 +1,51 @@
+import { ReactComponent as Add } from "@assets/add.svg";
+import { ReactComponent as Apple } from "@assets/apple.svg";
+import { ReactComponent as Article } from "@assets/article.svg";
+import { ReactComponent as Avatar } from "@assets/avatar.svg";
+import { ReactComponent as Brush } from "@assets/brush.svg";
+import { ReactComponent as Close } from "@assets/close.svg";
+import { ReactComponent as Comment } from "@assets/comment.svg";
+import { ReactComponent as Dashboard } from "@assets/dashboard.svg";
+import { ReactComponent as DropDown } from "@assets/drop-down.svg";
+import { ReactComponent as Error } from "@assets/error.svg";
+import { ReactComponent as Facebook } from "@assets/add.svg";
+import { ReactComponent as Google } from "@assets/google.svg";
+import { ReactComponent as Heart } from "@assets/heart.svg";
+import { ReactComponent as Like } from "@assets/like.svg";
+import { ReactComponent as Logo } from "@assets/logo.svg";
+import { ReactComponent as Logout } from "@assets/logout.svg";
+import { ReactComponent as Moon } from "@assets/moon.svg";
+import { ReactComponent as Notification } from "@assets/notification.svg";
+import { ReactComponent as Options } from "@assets/options.svg";
+import { ReactComponent as Plus } from "@assets/plus.svg";
+import { ReactComponent as Search } from "@assets/search.svg";
+import { ReactComponent as Send } from "@assets/send.svg";
+import { ReactComponent as Settings } from "@assets/settings.svg";
+import { ReactComponent as Upload } from "@assets/upload.svg";
+
+export {
+  Add,
+  Apple,
+  Article,
+  Avatar,
+  Brush,
+  Close,
+  Comment,
+  Dashboard,
+  DropDown,
+  Error,
+  Facebook,
+  Google,
+  Heart,
+  Logo,
+  Like,
+  Logout,
+  Moon,
+  Notification,
+  Options,
+  Plus,
+  Search,
+  Send,
+  Settings,
+  Upload,
+};
