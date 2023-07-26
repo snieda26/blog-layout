9a86dee3d8d78382417b7ffff66cd7170a51e975
serhiyhvala <serhiyhvala.code@gmail.com>
2023-07-26 14:14:25 +0300

    Update card component

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -4,32 +4,23 @@ import styles from "./card.module.scss";
 import { Button } from "ui/Button";
 import { Link } from "react-router-dom";
 import { Like } from "ui/Like";
+import { cropText } from "@utils/cropText";
+import { IArticlesData } from "@mock/articles";
+import { Tags } from "@components/Tags";
 
-interface ICard {
-  image: string;
-  tags: string[];
-  title: string;
-  description: string;
-}
+interface ICard extends IArticlesData {}
 
-export const Card: FC<ICard> = ({ image, tags, title, description }) => {
+export const Card: FC<ICard> = ({ id, image, tags, title, description }) => {
   return (
     <div className={styles.card}>
       <img src={image} alt="" className={styles.image} />
       <div className={styles.container}>
-        <div className={styles.tags}>
-          {tags.map(item => (
-            <div key={item} className={styles.tag}>
-              <span className={styles.hashtag}>#</span>
-              {item}
-            </div>
-          ))}
-        </div>
+        <Tags tags={tags} />
         <span className={styles.title}>{title}</span>
-        <span className={styles.description}>{description}</span>
+        <span className={styles.description}>{cropText(description, 120)}</span>
         <div className={styles.buttons}>
           <Like />
-          <Link to="/">
+          <Link to={`/articles/${id}`}>
             <Button borderRadius={4}>Прочитати</Button>
           </Link>
         </div>
