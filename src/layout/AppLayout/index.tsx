0103c4e2338a09c27a8d3d1670bb7f77f4279449
serhiyhvala <serhiyhvala.code@gmail.com>
2023-07-27 12:39:08 +0300

    Update App Layout

--- src/layout/AppLayout/index.tsx
+++ src/layout/AppLayout/index.tsx
@@ -5,12 +5,15 @@ import { Sidebar } from "@components/Sidebar";
 import styles from "./appLayout.module.scss";
 import { useContext } from "react";
 import { ThemeContext } from "@providers/ThemeProvider";
+import { MobileMenu } from "@components/MobileMenu";
 
 export const AppLayout = () => {
   const { currentTheme } = useContext(ThemeContext);
+
   return (
     <main className={styles.wrapper} data-theme={currentTheme}>
       <div className={styles.container}>
+        <MobileMenu />
         <Sidebar />
         <Outlet />
       </div>
